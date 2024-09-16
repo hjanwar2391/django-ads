d7fdc0efcb7401e55c30064721e78d81969b66f2
ANWAR HOSSEN <67516612+hjanwar2391@users.noreply.github.com>
2024-09-16 16:13:03 +0600

    same case slove

--- Ads_project/settings.py
+++ Ads_project/settings.py
@@ -129,21 +129,25 @@
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv() 
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ["*"]
 
 # CSRF Trusted Origins
-CSRF_TRUSTED_ORIGINS = ['https://webadscenter.onrender.com', 'http://127.0.0.1:8000']
+CSRF_TRUSTED_ORIGINS = ['https://webadscenter.onrender.com', 'http://127.0.0.1']
 
 
 # Static files
